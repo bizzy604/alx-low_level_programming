8238b3eab0cf09ff24c780ea9537ff104af815af
amoni kevin <kevinyamatta50@gmail.com>
2023-08-10 11:03:08 -0700

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - my main function here
+ *
+ * Return: if its 0 return succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
