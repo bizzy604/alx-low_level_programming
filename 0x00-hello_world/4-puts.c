2cb1b496168c41e4f6e457888a734cf71d4ec449
amoni kevin <kevinyamatta50@gmail.com>
2023-08-10 10:57:33 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - my first main function
+ *
+ * Return: return 0 if succesful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
