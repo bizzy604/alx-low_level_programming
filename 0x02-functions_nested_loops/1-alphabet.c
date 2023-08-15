7d616ff09a8d649692cd5a72e440a252c85c6b8e
amoni kevin <kevinyamatta50@gmail.com>
2023-08-15 04:05:07 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - printing alphabet in small letters
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
