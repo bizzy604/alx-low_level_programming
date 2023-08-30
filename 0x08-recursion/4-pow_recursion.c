f0bb8164d8695a0ac02691378397e613712b71fc
amoni kevin <kevinyamatta50@gmail.com>
2023-08-30 11:41:43 -0700

    new

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - powers using recursion
+ * @x: base value
+ * @y: power
+ * Return: Return y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
