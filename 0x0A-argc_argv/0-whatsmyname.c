c33cba621b3e9196f8e5e0f5eb5202fb1d9c9a2d
amoni kevin <kevinyamatta50@gmail.com>
2023-09-01 11:11:49 -0700

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing the name
+ * @argc: argunent count
+ * @argv: argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s \n", argv[0]);
+	return (0);
+}
