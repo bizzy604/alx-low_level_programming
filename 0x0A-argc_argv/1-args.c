d4596e05f7af5d58e0226410906e469005a1df8c
amoni kevin <kevinyamatta50@gmail.com>
2023-09-01 12:05:26 -0700

    new

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,10 @@
  * @argv: argument array
  * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	printf("%d \n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
