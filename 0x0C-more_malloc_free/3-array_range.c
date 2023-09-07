e724981ec2fadab958e8c38bd3298c33c18cc901
amoni kevin <kevinyamatta50@gmail.com>
2023-09-07 14:22:05 -0700

    new

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * array_range - arranging of array
+ * @min: minium parameter
+ * @max: maximum parameter
+ * Return: pointer
+ */
+int *array_range(int min, int max)
+{
+	int *ptrA;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	ptrA = malloc(sizeof(int) * size);
+	if (ptrA == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+		ptrA[i] = min++;
+	return (ptrA);
+}
