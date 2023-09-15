9d041850170839052070723e45e26dfb6ac7db23
amoni kevin <kevinyamatta50@gmail.com>
2023-09-15 07:40:25 -0700

    new

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,98 +1,51 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stddef.h>
-/**
- * print_char - function prints char
- * @arg: argument
- */
-
-void print_char(va_list arg)
-{
-	char c = va_arg(arg, int);
-
-	printf("%c", c);
-}
 
 /**
- * print_int - function prints int
- * @arg: argument
- */
-
-void print_int(va_list arg)
-{
-	int i = va_arg(arg, int);
-
-	printf("%d", i);
-}
-
-/**
- * print_float - function prints float
- * @arg: argument
- */
-
-void print_float(va_list arg)
-{
-	float f = va_arg(arg, double);
-
-	printf("%f", f);
-}
-
-/**
- * print_string - function prints strings.
- * @arg: argument
- */
-
-void print_string(va_list arg)
-{
-	char *s = va_arg(arg, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-
-/**
- * print_all - function that prints anything.
- * @format: format specifier
+ * print_all - Prints anything
+ * @format: specifies the necessary operations
+ * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
-	va_list arg;
-	int i, j;
-	char *sep = "";
-	
-	char *data[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
+	int i;
+	int kev;
+	char *str;
+	va_list my_list;
 
-	va_start(arg, format);
-
+	va_start(my_list, format);
 	i = 0;
-	while (format && *(format + i))
+	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-		while (j < 4 && *(format + i) != *(data[j]))
+		switch (format[i])
 		{
-			j++;
+			case 'c':
+				printf("%c", va_arg(my_list, int));
+				kev = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(my_list, int));
+				kev = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(my_list, double));
+				kev = 0;
+				break;
+			case 's':
+				str = va_arg(my_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				kev = 0;
+				break;
+			default:
+				kev = 1;
+				break;
 		}
-
-		if (j < 4)
-		{
-			printf("%s", sep);
-			data[j].f_pr(arg);
-			sep = ", ";
-		}
+		if (format[i + 1] != '\0' && kev == 0)
+			printf(", ");
 		i++;
 	}
 	printf("\n");
-
-	va_end(arg);
+	va_end(my_list);
 }
