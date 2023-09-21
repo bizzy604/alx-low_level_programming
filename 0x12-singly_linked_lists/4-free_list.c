8ab1d3f50dc88d8e35637c84fb4606e9b5bb480c
amoni kevin <kevinyamatta50@gmail.com>
2023-09-21 11:06:33 -0700

    new

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - memory  release allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
